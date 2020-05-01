0b9b1f0a0d4200a8c24865d6c03d1920d8eedeb3
naitalamara <ouali.naitalamara@gmail.com>
2020-05-01 17:27:49 +0200

    creation de la classe Personnel_GroupeDaoJdbc

---
+++ exo4_1/src/main/java/uvsq21807569/exo4_1/Groupe_PersonnelDaoJdbc.java
@@ -0,0 +1,29 @@
+package uvsq21807569.exo4_1;
+
+public class Groupe_PersonnelDaoJdbc implements DAO<Groupe_Personnel> {
+
+	@Override
+	public Groupe_Personnel create(Groupe_Personnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Groupe_Personnel read(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Groupe_Personnel update(Groupe_Personnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Groupe_Personnel obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
