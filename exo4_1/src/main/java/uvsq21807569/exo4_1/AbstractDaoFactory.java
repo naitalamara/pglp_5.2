cc262fe0ec9410b9d49e98e86e791a753846dc7e
naitalamara <ouali.naitalamara@gmail.com>
2020-05-04 20:14:04 +0200

    creation de la classe JdbcDaofactory

--- exo4_1/src/main/java/uvsq21807569/exo4_1/AbstractDaoFactory.java
+++ exo4_1/src/main/java/uvsq21807569/exo4_1/AbstractDaoFactory.java
@@ -1,5 +1,26 @@
 package uvsq21807569.exo4_1;
 
 public abstract class AbstractDaoFactory {
+public enum DAOType {JDBC, Serialiser};
+
+	
+
+	public abstract DAO<Personnel> getPersonnelDAO();
+
+	
+
+	public abstract DAO<Groupe_Personnel> getPersonnelGroupeDAO();
+
+	
+
+	public static AbstractDaoFactory getFactory(DAOType type) {
+
+		if (type == DAOType.JDBC) return new JdbcDaoFactory();
+
+		if (type == DAOType.Serialiser) return new DAOFactory();
+
+		return null;
+
+	}
 
 }
--- exo4_1/src/main/java/uvsq21807569/exo4_1/JdbcDaoFactory.java
+++ exo4_1/src/main/java/uvsq21807569/exo4_1/JdbcDaoFactory.java
@@ -1,19 +1,17 @@
 package uvsq21807569.exo4_1;
 
-public class DAOFactory extends AbstractDaoFactory{
-	
-	
-	
+public class JdbcDaoFactory extends AbstractDaoFactory {
 
 	@Override
 	public DAO<Personnel> getPersonnelDAO() {
-		return new PersonnelDAO( ) ;
-		
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public DAO<Groupe_Personnel> getPersonnelGroupeDAO() {
-		return new Groupe_PersonnelDAO( ) ;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
